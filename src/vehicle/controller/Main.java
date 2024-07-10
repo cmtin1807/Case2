63b0500872b5ca2aa6c72e12b71877519b27897e
cmtin1807 <cmtin1807@gmail.com>
2024-07-10 10:53:14 +0700

    hello5

--- src/vehicle/controller/Main.java
+++ src/vehicle/controller/Main.java
@@ -1,47 +1,11 @@
 package vehicle.controller;
 
 
-import java.util.Scanner;
-
-import static vehicle.controller.MenuVehicle.*;
-import static vehicle.service.FactoryVehicle.*;
-import static vehicle.template.DisplayProgram.displayMenu;
-import static vehicle.util.IViewMenu.*;
-import static vehicle.util.ReadAndWrite.*;
-
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        loadFileTruck(fileTruck, truckManager);
-        loadFileCar(fileCar, carManager);
-        loadFileMotorBike(fileMotorBike, motorBikeManager);
-        while (true) {
-            int choice = displayMenu(scanner);
-            switch (choice) {
-                case ADD_VEHICLE:
-                    functionAddVehicle(scanner);
-                    break;
-                case SHOW_VEHICLE :
-                    functionDisplayVehicle(scanner);
-                    break;
-                case REMOVE_VEHICLE:
-                    functionRemoveVehicle(scanner, fileTruck, truckManager, carManager, fileCar, motorBikeManager);
-                    break;
-                case UPDATE_VEHICLE:
-                    functionUpdateVehicle(scanner, fileTruck, truckManager, carManager, fileCar, motorBikeManager);
-                    break;
-                case SORT_VEHICLE:
-                    functionSortVehicle(scanner);
-                    break;
-                case EXIT:
-                    System.exit(0);
-                default:
-                    System.out.println("Lựa chọn của bạn không hợp lệ. Vui lòng chọn lại.");
-            }
-        }
+        MenuVehicle menuVehicle = new MenuVehicle();
+        menuVehicle.runProgram();
     }
-
 
 
 
--- src/vehicle/controller/MenuVehicle.java
+++ src/vehicle/controller/MenuVehicle.java
@@ -22,13 +22,47 @@ import static vehicle.util.IViewAddVehicle.*;
 import static vehicle.util.IViewAddVehicle.EXIT_ADD;
 import static vehicle.util.IViewDisplayVehicle.*;
 import static vehicle.util.IViewDisplayVehicle.EXIT_DISPLAY;
+import static vehicle.util.IViewMenu.*;
+import static vehicle.util.IViewMenu.EXIT;
 import static vehicle.util.IViewRemoveVehicle.*;
 import static vehicle.util.IViewRemoveVehicle.EXIT_REMOVE;
 import static vehicle.util.IViewSortVehicle.*;
 import static vehicle.util.IViewUpdateVehicle.*;
 import static vehicle.util.IViewUpdateVehicle.EXIT_UPDATE;
+import static vehicle.util.ReadAndWrite.*;
 
 public class MenuVehicle {
+    public void runProgram() {
+        Scanner scanner = new Scanner(System.in);
+        loadFileTruck(fileTruck, truckManager);
+        loadFileCar(fileCar, carManager);
+        loadFileMotorBike(fileMotorBike, motorBikeManager);
+        while (true) {
+            int choice = displayMenu(scanner);
+            switch (choice) {
+                case ADD_VEHICLE:
+                    functionAddVehicle(scanner);
+                    break;
+                case SHOW_VEHICLE :
+                    functionDisplayVehicle(scanner);
+                    break;
+                case REMOVE_VEHICLE:
+                    functionRemoveVehicle(scanner, fileTruck, truckManager, carManager, fileCar, motorBikeManager);
+                    break;
+                case UPDATE_VEHICLE:
+                    functionUpdateVehicle(scanner, fileTruck, truckManager, carManager, fileCar, motorBikeManager);
+                    break;
+                case SORT_VEHICLE:
+                    functionSortVehicle(scanner);
+                    break;
+                case EXIT:
+                    System.exit(0);
+                default:
+                    System.out.println("Lựa chọn của bạn không hợp lệ. Vui lòng chọn lại.");
+            }
+        }
+    }
+
     public static void functionAddVehicle(Scanner scanner){
         boolean addVehicle = true;
         while (addVehicle) {
