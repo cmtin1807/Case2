3a80b0f1342aaa94ca78011c0cf0e00ce91f34d1
cmtin1807 <cmtin1807@gmail.com>
2024-07-11 14:15:33 +0700

    hello6

--- src/vehicle/service/FactoryVehicle.java
+++ src/vehicle/service/FactoryVehicle.java
@@ -8,20 +8,10 @@ import vehicle.model.Truck;
 
 
 import java.io.File;
-import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-
-import static vehicle.service.CarManager.*;
-import static vehicle.service.MotorBikeManager.*;
-import static vehicle.service.TruckManager.*;
-import static vehicle.template.DisplayProgram.*;
-import static vehicle.util.IViewAddVehicle.*;
-import static vehicle.util.IViewDisplayVehicle.*;
-import static vehicle.util.IViewRemoveVehicle.*;
-import static vehicle.util.IViewUpdateVehicle.*;
 
 
 public class FactoryVehicle {
@@ -35,9 +25,6 @@ public class FactoryVehicle {
     public static File fileTruck = new File(linkFileTruck);
     public static File fileCar = new File(linkFileCar);
     public static File fileMotorBike = new File(linkFileMotorBike);
-
-
-
 
 
 }
